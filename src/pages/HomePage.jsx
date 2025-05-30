684fe3f7fb114fdd5df22a6d490f91daaaeaa9d3
danirando <danielerando@tiscali.it>
2025-05-30 19:34:30 +0200

    uso useEffect per ottenere lettera selezionata e parola random

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,49 +1,65 @@
 import axios from "axios";
+import { useEffect, useState } from "react";
+
+const alphabet = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+const url = "https://random-word-api.herokuapp.com/word?lang=it";
+
+const wordArray = [];
 
 export default function HomePage() {
-  const url = "https://random-word-api.herokuapp.com/word?lang=it";
-  const wordArray = [];
-  const alphabet = [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",
-  ];
-  const fetchWorld = (url) => {
+  const [wordArray, setWordArray] = useState([]);
+  const [selectedLetter, setSelectedLetter] = useState(null);
+
+  const fetchWord = () => {
     axios.get(url).then((res) => {
-      return wordArray.push(res.data[0]);
+      const newWord = res.data[0];
+      setWordArray(...wordArray, newWord);
+      console.log(newWord);
     });
   };
-  fetchWorld(url);
-  console.log(wordArray);
+  useEffect(fetchWord, []);
+
+  const handleLetterClick = (letter) => {
+    setSelectedLetter(letter);
+    console.log(letter);
+  };
 
   return (
     <div className="container d-flex justify-content-around">
       {alphabet.map((letter, i) => (
-        <div key={i} className="letter-container">
-          {letter}
+        <div
+          onClick={() => handleLetterClick(letter)}
+          key={i}
+          className="letter-container">
+          {letter.toUpperCase()}
         </div>
       ))}
     </div>
