e1bb4aefbc5a67c5bc528a106600c90e0223f345
danirando <danielerando@tiscali.it>
2025-05-30 19:01:15 +0200

    chiamata axios per parola - array dell'alfabeto

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,51 @@
+import axios from "axios";
+
 export default function HomePage() {
+  const url = "https://random-word-api.herokuapp.com/word?lang=it";
+  const wordArray = [];
+  const alphabet = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+  const fetchWorld = (url) => {
+    axios.get(url).then((res) => {
+      return wordArray.push(res.data[0]);
+    });
+  };
+  fetchWorld(url);
+  console.log(wordArray);
+
   return (
-    <div>
-      <h1>Home page</h1>
+    <div className="container d-flex justify-content-around">
+      {alphabet.map((letter, i) => (
+        <div key={i} className="letter-container">
+          {letter}
+        </div>
+      ))}
     </div>
   );
 }
